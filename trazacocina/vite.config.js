3084a6573a83364e8eda7f64bc1a0e0c91c74a08
ricardo-09 <rictri24@gmail.com>
2026-08-20 00:20:42 -0400

    descargar app

--- trazacocina/vite.config.js
+++ trazacocina/vite.config.js
@@ -1,6 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'WammaCocina',
+        short_name: 'WammaCocina',
+        description: 'Control de insumos y consumos',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
